e025ea0170b33b2cdaa059536092eb235cd2558a
Saul Enrique <saulmallqui1999@gmail.com>
2023-09-21 22:13:25 -0500

    Se actualiza el proyecto

--- PrimerProyecto/Program.cs
+++ PrimerProyecto/Program.cs
@@ -1,4 +1,8 @@
-﻿//Usando el metodo System.Math
+﻿
+
+Console.WriteLine("-----Usando el metodo System.Math------");
+
+//Calculamos el numero mayor 
 int firstValue = 500;
 int secondValue = 600;
 int largerValue;
@@ -6,3 +10,16 @@ int largerValue;
 largerValue = Math.Max(firstValue,secondValue);
 
 Console.WriteLine(largerValue);
+
+
+Console.WriteLine("----------------------------------------");
+
+//Genramos un numero aleatorio
+
+Random numero = new Random();
+int roll1 = numero.Next(50);
+int roll2 = numero.Next(1,10);
+int roll3 = numero.Next();
+Console.WriteLine(roll1);
+Console.WriteLine(roll2);
+Console.WriteLine(roll3);
