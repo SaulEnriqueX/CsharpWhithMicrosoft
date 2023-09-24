597ee4f9586df61b81348dd7075aa7d6d3cd63b1
Saul Enrique <saulmallqui1999@gmail.com>
2023-09-23 23:20:20 -0500

    Se sube el septimo proyecto

---
+++ SeptimoProyecto/Program.cs
@@ -0,0 +1,13 @@
+﻿// Reto con matrices
+
+
+
+string[] pedidos = {"B123","C234","A345","C15","B177","G3003","C235","B179"};
+foreach (string pedido in pedidos)
+{
+    if(pedido.StartsWith("B"))
+    {
+         Console.WriteLine($"The name starts {pedido} with 'B'!");
+    }
+   
+}
